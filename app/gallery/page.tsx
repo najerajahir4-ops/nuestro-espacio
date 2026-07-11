e01e6d835d449f520fcbc66ec2be6652858f41df
Kenny Nájera <najerajahir4@gmail.com>
2026-07-11 00:57:02 -0500

    fix: restore missing Image import in gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -9,7 +9,7 @@ import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
 
 import { GalleryLightbox, MediaItem } from '@/components/GalleryLightbox';
-
+import Image from 'next/image';
 
 interface AlbumItem {
   id: string;
